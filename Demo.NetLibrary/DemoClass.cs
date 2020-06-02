06eef757d3c81134d43780895f9acf3b29052ea5
ttkoma <k.lapeev@gmail.com>
2020-06-03 02:12:58 +0300

    Add project files.

---
+++ Demo.NetLibrary/DemoClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Demo.NetLibrary
+{
+    public class DemoClass
+    {
+        public string UserName { get; set; }
+
+        public DemoClass(string username)
+        {
+            this.UserName = username;
+        }
+    }
+}
---
+++ Demo.NetLibraryTests/DemoClassTests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+using Demo.NetLibrary;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+
+namespace Demo.NetLibrary.Tests
+{
+    public class DemoClassTests
+    {
+        [Fact()]
+        public void DemoClassTest()
+        {
+            var instance = new DemoClass("ttkoma");
+
+            instance.Should().NotBeNull();
+            instance.UserName.Should().Be("ttkoma");
+        }
+    }
+}
